c67c29b5c177e4ed9b6df9d3dc3318a6bae6bca2
Yiyeon Kim <59812671+ykim879@users.noreply.github.com>
2020-05-12 15:00:20 -0700

    Create Dialog.py

---
+++ gui using wxPython/Dialog.py
@@ -0,0 +1,38 @@
+import wx
+
+class MyDialog(wx.Dialog):
+	def __init__(self, _parent, _title):
+		wx.Dialog.__init__(self, parent = _parent, title = _title)
+		self.mainPanel = wx.Panel(self)
+		self.closeButton = wx.Button(self.mainPanel, label = "Close")
+		self.Bind(wx.EVT_BUTTON, self.OnClose, self.closeButton)
+		self.Bind(wx.EVT_CLOSE, self.OnClose)
+	def OnClose(self, e):
+		self.Destroy()
+class MyFrame(wx.Frame):
+	def __init__(self):
+		wx.Frame.__init__(self, parent = None, title = "Modeal & Modeless")
+		self.mainPanel = wx.Panel(self)
+		self.mainSizer = wx.BoxSizer(wx.HORIZONTAL)
+		self.mainPanel.SetSizer(self.mainSizer)
+
+		self.modal = wx.Button(self.mainPanel, label = "Modal")
+		self.modeless = wx.Button(self.mainPanel, label = "Modeless")
+
+		self.mainSizer.Add(self.modal, 0, wx.ALIGN_LEFT|wx.ALL, 5)
+		self.mainSizer.Add(self.modeless, 0, wx.ALIGN_RIGHT|wx.ALL, 5)
+
+		self.Bind(wx.EVT_BUTTON, self.Modal, self.modal)
+		self.Bind(wx.EVT_BUTTON, self.Modeless, self.modeless)
+
+	def Modal(self, e):
+		dlg = MyDialog(self, "Modal Dialog")
+		dlg.ShowModal()
+	def Modeless(self, e):
+		dlg = MyDialog(self, "Modeless Dialog")
+		dlg.Show()
+if __name__ == "__main__":
+	app = wx.App()
+	frame = MyFrame()
+	frame.Show()
+	app.MainLoop()
